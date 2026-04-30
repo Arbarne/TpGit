ced99daf8cd0c50cbc34d15f12fa8141841acbdf
arbarne <moulardbaptiste@outlook.fr>
2026-04-30 12:30:35 +0200

    ajout de 5 jeux

--- ajc-nintendo-main/NintendoProject/src/test/java/nintendo/test/Test.java
+++ ajc-nintendo-main/NintendoProject/src/test/java/nintendo/test/Test.java
@@ -1,10 +1,19 @@
 package nintendo.test;
 
+import nintendo.model.Console;
+import nintendo.model.Jeu;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Console gamecube = new Console("Gamecube");
+		Console wii = new Console("wii");
+		Jeu j1 = new Jeu("wii sport", wii);
+		Jeu j2 = new Jeu("supermariowii", wii);
+		Jeu j3 = new Jeu("mario sushine", gamecube);
+		Jeu j4 = new Jeu("f-zero", gamecube);
+		Jeu j5 = new Jeu("zelda twilight princess", wii);
 	}
 
 }
